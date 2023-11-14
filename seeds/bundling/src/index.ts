8eb3d72d2fcfaf4faf8a89b886f523d05641e5ff
Matthew Soulanille <msoulanille@google.com>
2023-11-14 15:51:11 -0800

    Bundle types into a single '.d.ts' file using api-extractor

--- seeds/bundling/src/index.ts
+++ seeds/bundling/src/index.ts
@@ -1,11 +1,18 @@
 import {Board, Node, LogProbe, DebugProbe, RunResult} from '@google-labs/breadboard';
 import {Starter} from '@google-labs/llm-starter';
 
-(window as any)['breadboard'] = {
+export * from '@google-labs/breadboard';
+export * from '@google-labs/llm-starter';
+
+export type Foo = string;
+
+export const breadboard = {
   Board,
   Node,
   LogProbe,
   DebugProbe,
   RunResult,
   Starter,
-}
+};
+
+(window as any)['breadboard'] = breadboard;
